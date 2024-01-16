e15834e29ffce06e3e6031857dd5a8a8ebebd01f
Markinmarch <iztvmrk@mail.ru>
2024-01-16 21:57:13 +0300

    config -> refactory

--- main.py
+++ main.py
@@ -8,13 +8,18 @@ def logger():
         with open('ZTE_point.log', mode = 'x', encoding = 'utf-8') as log_file:
             log_file.close()
     else:
-        callable(logging.basicConfig(
-            filename = f'ZTE_point.log',
-            filemode = 'w',
-            format = '%(asctime)s %(levelname)s %(message)s',
-            level = logging.INFO
-        ))
+        pass
+
+    logging.basicConfig(
+        format = '%(asctime)s %(name)s %(levelname)s %(message)s',
+        level = logging.INFO,
+        handlers = [
+            logging.FileHandler('ZTE_point.log'),
+            logging.StreamHandler()
+        ]
+    )
 
 def main():
-    from DB import session
+    from DB import session, create_database
+    create_database
     session
