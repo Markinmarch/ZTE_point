c110b399ddff16cd99c87359f23f4d495125e3d2
Markinmarch <iztvmrk@mail.ru>
2024-01-20 12:06:03 +0300

    DB -> add db_main

--- DB/__init__.py
+++ DB/__init__.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-from .setting import DataBase
+from .db_config import DataBase
 
 
 load_dotenv(dotenv_path = '.env')
---
+++ DB/db_main.py
@@ -0,0 +1,18 @@
+def insert_user():
+    pass
+
+def select_user():
+    pass
+
+def delete_user():
+    pass
+
+def inser_item():
+    pass
+
+def select_item():
+    pass
+
+def add_order():
+    pass
+
