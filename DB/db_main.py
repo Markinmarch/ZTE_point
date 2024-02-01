2b9a12df046aee3f3ac2a64d2e7abd48686a3c2c
Markinmarch <iztvmrk@mail.ru>
2024-02-01 21:36:14 +0300

    add insert user func

--- DB/db_main.py
+++ DB/db_main.py
@@ -1,8 +1,26 @@
-def insert_user():
-    pass
+from . import session
+from .db_models import User, Item, Order
 
-def select_user():
-    pass
+
+def insert_user(
+    user_name: str,
+    user_phone: str,
+    user_email: str,
+    user_password: str
+) -> None:
+    session.add(
+        User(
+            name = user_name,
+            phone = user_phone,
+            email = user_email,
+            password = user_password
+    ))
+    session.commit()
+    session.close()
+
+def select_user(user_id: int) -> str:
+    return session.query(User).filter(User.id == user_id)
+            
 
 def delete_user():
     pass
