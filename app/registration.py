bc8ee43d5db1351eea242f63659fe841f6f04ce6
Markinmarch <iztvmrk@mail.ru>
2024-02-01 21:35:49 +0300

    add registration

--- app/registration.py
+++ app/registration.py
@@ -1,11 +1,15 @@
 from flask import request, render_template
-from .app_settings import engine
+
+from .app_settings import dp
+from DB.db_main import insert_user
 
 
-@engine.route('/registration', methods = ['POST', 'GET'])
+@dp.route('/registration', methods = ['POST', 'GET'])
 def registration():
-    user_name = request.form['userName']
-    user_phone = request.form['userPhone']
-    user_email = request.form['userEmail']
-    user_password = request.form['userPassword']
+    insert_user(
+        user_name = request.form['userName'],
+        user_phone = request.form['userPhone'],
+        user_email = request.form['userEmail'],
+        user_password = request.form['userPassword']
+    )
     return render_template('registration.html')
