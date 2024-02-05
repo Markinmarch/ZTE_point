aceffda638faf9c274917c4187b7d7ca431967a0
Markinmarch <iztvmrk@mail.ru>
2024-02-06 01:16:33 +0300

    registration -> debug  JS + refactory py

--- app/registration.py
+++ app/registration.py
@@ -7,11 +7,13 @@ from DB.db_main import insert_user
 @dp.route('/registration', methods = ['POST', 'GET'])
 def registration():
     if request.method == 'POST':
+        if request.form['userPassword'] != request.form['repeatPassword']:
+            return render_template('registration.html')
         insert_user(
             user_name = request.form['userName'],
             user_phone = request.form['userPhone'],
             user_email = request.form['userEmail'],
-            user_password = request.form['userPassword']
+            user_password = request.form['userPassword'],
         )
         return render_template('home_page.html')
     else:
--- static/success_registration.js
+++ static/success_registration.js
@@ -1,49 +1,34 @@
-// window.onload = addUser;
+window.onload = addUser;
 
 function addUser() {
     var buttonName = document.getElementById("addUser");
     buttonName.onclick = handleButtonClick;
+    // buttonName.onclick = checkCorrectUserPhone;
 }
 
 function handleButtonClick() {
-    var textInputName = document.getElementsByName("userName");
-    var userName = textInputName.value;
-    var textInputPhone = document.getElementsByName("userPhone");
-    var userPhone = textInputPhone.value;
-    var textInputEmail = document.getElementsByName("userEmail");
-    var userEmail = textInputEmail.value;
-    var textInputPassword = document.getElementsByName("userPassword");
-    var userPassword = textInputPassword.value;
-    var textInputRepeatPassword = document.getElementsByName("repeatPassword");
-    var repeatPassword = textInputRepeatPassword.value;
-    if (userName === undefined || userPhone === undefined || userEmail === undefined || userPassword === undefined) {
-        alert("Введите все параметры, пожалуйста!");
+    var userName = document.getElementById("userName").value;
+    var userPhone = document.getElementById("userPhone").value;
+    var userEmail = document.getElementById("userEmail").value;
+    var userPassword = document.getElementById("userPassword").value;
+    var repeatPassword = document.getElementById("repeatPassword").value;
+
+    if (userName == "" || userPhone == "" || userEmail == "" || userPassword == "") {
+        alert("Введите параметры, пожалуйста!");
     }
-    if (repeatPassword !== userPassword) {
+    else if (repeatPassword !== userPassword) {
         alert("Пароли не совпадают! Повторите попытку!");
     }
     else {
-        alert("Поздравляем, " + userName + ", " + "регистрация прошла успешно!");
+        alert("Ваше имя: " + userName + " Ваш возраст: " + userPhone);
     }
 }
 
+// function checkCorrectUserPhone() {
+//     var userPhone = document.getElementById("userPhone").value;
+//     var checkPhone = userPhone.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im);
 
-// window.onload = addUser;
-
-// function addUser() {
-//     var buttonName = document.getElementById("addParams");
-//     buttonName.onclick = handleButtonClick;
-// }
-
-// function handleButtonClick() {
-//     var textInputName = document.getElementById("userName");
-//     var userName = textInputName.value;
-//     var textInputAge = document.getElementById("userAge");
-//     var userAge = textInputAge.value;
-//     if (userName == "" || userAge == "") {
-//         alert("Введите параметры, пожалуйста!");
-//     }
-//     else {
-//         alert("Ваше имя: " + userName + " Ваш возраст: " + userAge);
+//     if (checkPhone === false) {
+//         alert("Номер телефона введён некорректно. Повторите попытку.");
 //     }
 // }
