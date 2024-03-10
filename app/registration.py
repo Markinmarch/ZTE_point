0768509c2b25a32418811e1f0b38b2d6b6869f9d
Markinmarch <iztvmrk@mail.ru>
2024-03-10 03:37:36 +0300

    upd: check email from database

--- app/registration.py
+++ app/registration.py
@@ -1,5 +1,4 @@
-from flask import request, render_template, redirect, jsonify, after_this_request, stream_template_string
-import json
+from flask import request, render_template, redirect
 
 from .app_settings import dp
 from DB.models import User
@@ -8,20 +7,18 @@ from DB.models import User
 @dp.route('/registration', methods = ['GET', 'POST'])
 def registration():
     if request.method == 'POST':
-        # name = request.form['userName']
-        # phone = request.form['userPhone']
-        # email = request.form['userEmail']
-        # password = request.form['userPassword']
-        email = request.form["email"]
-        print(email)
+        user_datas = request.json
+        email = user_datas['email']
+        name = user_datas['name']
+        phone = user_datas['phone']
+        password = user_datas['password']
         if User.check_email(email) == True:
-            return jsonify('email_is_busy')
-        # else:
-        #     User.insert_user(
-        #         user_name = name,
-        #         user_phone = phone,
-        #         user_email = email,
-        #         user_password = password
-        #     )
-        #     return redirect('/')
+            return 'false'
+        else:
+            User.insert_user(
+                user_name = name,
+                user_phone = phone,
+                user_email = email,
+                user_password = password
+            )
     return render_template('registration.html')
--- static/registration.js
+++ static/registration.js
@@ -2,16 +2,23 @@ window.onload = addUser;
 
 function addUser() {
     var buttonName = document.getElementById("addUser");
-    buttonName.onclick = handleButtonClick;
+    buttonName.onclick = getUserRegistration;
 }
 
-function handleButtonClick() {
+function getUserRegistration() {
 
     var userName = document.getElementById("userName").value;
     var userPhone = document.getElementById("userPhone").value;
     var userEmail = document.getElementById("userEmail").value;
     var userPassword = document.getElementById("userPassword").value;
     var repeatPassword = document.getElementById("repeatPassword").value;
+
+    var userData = JSON.stringify({
+        email: userEmail,
+        name: userName,
+        phone: userPhone,
+        password: userPassword
+    });
 
     if (userName == "" || userPhone == "" || userEmail == "" || userPassword == "") {
         alert("Введите параметры, пожалуйста!");
@@ -23,7 +30,21 @@ function handleButtonClick() {
         clearRepeatPassword.value = "";
         return false;
     }
-    else {
-        alert("Поздравляем, " + userName + ", регистрация завершена!")
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/registration", true);
+    xhr.setRequestHeader('content-type', 'application/json; charset=UTF-8');
+    xhr.send(userData);
+    xhr.onload = function() {
+        if (xhr.response == "false") {
+            alert("Адрес почты "+ userEmail + " уже зарегестрирован. Используйте другой.");
+            var clearUserEmail = document.getElementById("userEmail");
+            clearUserEmail.value = "";
+        }
+        else {
+            alert("Поздравляем, " + userName + ", регистрация завершена!");
+            window.location.replace("http://localhost:5000");
+        }
     }
+    
 }
