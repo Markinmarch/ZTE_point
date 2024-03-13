4276e9f4dfa38851ec094476bb727f643fa57e85
Markinmarch <iztvmrk@mail.ru>
2024-03-13 22:34:17 +0300

    edit config.py

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
+import binascii
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path = '.env')
 
-postgres_password = os.getenv('POSTGRES_PASSWORD', '')
-postgres_user = 'postgres'
-postgres_host = 'localhost'
-postgres_port = '5432'
-postgres_database = 'main_db'
+SECRET_WORD = os.getenv('SECRET_WORD', '').encode("utf-8").hex()
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', '')
+POSTGRES_USER = 'postgres'
+POSTGRES_HOST = 'localhost'
+POSTGRES_PORT = '5432'
+POSTGRES_DATABASE = 'main_db'
