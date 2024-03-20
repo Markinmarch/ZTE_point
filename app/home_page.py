9ce3a12aa348537d599e26bc3e10af04253ca2ce
Markinmarch <iztvmrk@mail.ru>
2024-03-20 22:24:44 +0300

    visual changes after logging in

--- app/home_page.py
+++ app/home_page.py
@@ -1,7 +1,23 @@
 from flask import render_template
-from .app_settings import dp
+from flask_login import current_user
 
+from .app_settings import dp, login_manager
+from DB.models import User
+
+
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.get_user(user_id)
 
 @dp.route('/')
 def home():
-    return render_template('home_page.html')
+    if current_user.is_authenticated:
+        data = [{
+            'context': '<a class = "login" type = "button" href = "/login">Выйти</a><a class = "bascket" type = "button" href = "/bascket"><img src="https://t700.ru/icon/cart.png" width="42" height="42"></a>'
+        }]
+    else:
+        data = [{
+            'context': '<a class = "login" type = "button" href = "/login">Войти</a>'
+        }]
+    return render_template('home_page.html', data = data)
+    # return render_template('home_page.html')
