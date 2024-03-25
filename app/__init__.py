fd8327e089badd664809b92c7d443bd3685eeec4
Markinmarch <iztvmrk@mail.ru>
2024-03-25 21:01:31 +0300

    add logout user func

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,4 @@ from . import home_page
 from . import login_auth
 from . import items
 from . import registration
+from . import logout
---
+++ app/logout.py
@@ -0,0 +1,15 @@
+from flask import request, render_template, redirect
+from flask_login import logout_user
+
+from .app_settings import dp, login_manager
+from DB.models import User
+
+
+@login_manager.user_loader
+def get_logout_user(user_id):
+    return User.get_user(user_id)
+
+@dp.route('/logout')
+def logout():
+    logout_user()
+    return redirect('/')
