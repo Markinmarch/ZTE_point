26be7b1f724cb42cd831bee17c09bbf48ed91f05
Markinmarch <iztvmrk@mail.ru>
2024-04-09 23:15:57 +0300

    publishing product data from the database

--- app/items.py
+++ app/items.py
@@ -2,10 +2,11 @@ from flask import request, render_template
 from flask_login import login_required
 
 from app.app_settings import dp, login_manager
-from DB.models import User, session
+from DB.models import Item, User, session
 
 
 @dp.route('/items', methods = ['GET', 'POST'])
 @login_required
 def items():
-    return render_template('shop.html')
+    items = Item.get_items()
+    return render_template('items.html', data = items)
