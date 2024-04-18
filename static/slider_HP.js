53cc19ea9539f42c74478b206554375b8c9fe070
Markinmarch <iztvmrk@mail.ru>
2024-04-18 23:26:08 +0300

    upd

--- static/slider_HP.js
+++ static/slider_HP.js
@@ -1,36 +1,28 @@
-const slider = document.querySelector('.slider');
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
-const slides = Array.from(slider.querySelectorAll('img'));
-const slideCount = slides.length;
-let slideIndex = 0;
+const images = document.querySelectorAll('.slider img');
+let currentIndex = 0;
 
-// Устанавливаем обработчики событий для кнопок
-prevButton.addEventListener('click', showPreviousSlide);
-nextButton.addEventListener('click', showNextSlide);
-
-// Функция для показа предыдущего слайда
-function showPreviousSlide() {
-  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
-  updateSlider();
+function showImage(index) {
+  images[currentIndex].classList.remove('active');
+  images[index].classList.add('active');
+  currentIndex = index;
 }
 
-// Функция для показа следующего слайда
-function showNextSlide() {
-  slideIndex = (slideIndex + 1) % slideCount;
-  updateSlider();
-}
-
-// Функция для обновления отображения слайдера
-function updateSlider() {
-  slides.forEach((slide, index) => {
-    if (index === slideIndex) {
-      slide.style.display = 'block';
-    } else {
-      slide.style.display = 'none';
+document
+  .querySelector('.slider.controlls')
+  .addEventListener('click', function (event) {
+    if (event.target.classList.contains('prev')) {
+      let index = currentIndex - 1;
+      if (index < 0) {
+        index = images.length - 1;
+      }
+      showImage(index);
+    } else if (event.target.classList.contains('next')) {
+      let index = currentIndex + 1;
+      if (index >= images.length) {
+        index = 0;
+      }
+      showImage(index);
     }
   });
-}
 
-// Инициализация слайдера
-updateSlider();
+showImage(currentIndex);
