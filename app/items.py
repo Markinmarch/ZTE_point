94f9e30461a69cc6be0e107be77d4a76cb91acc3
Markinmarch <iztvmrk@mail.ru>
2024-04-27 23:26:37 +0300

    debug

--- app/items.py
+++ app/items.py
@@ -11,11 +11,10 @@ def items():
     items = Item.get_items()
     return render_template('items.html', data = items)
 
-@dp.route('/items', methods = ['GET','POST'])
+@dp.route('/items', methods = ['GET', 'POST'])
 def search_items():
     words = request.form['itemSearch']
     words_list = [word.lower() for word in words.split()]
     keywords = set(words_list)
     items = Item.search_items(keyword = keywords)
-    print(items)
-    return render_template('items.html', data = items)
+    return render_template('items.html', search_items_data = items)
