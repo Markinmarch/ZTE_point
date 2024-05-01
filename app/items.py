6374464005e93ec38beeae51d1bbfa43deb00c3e
Markinmarch <iztvmrk@mail.ru>
2024-05-01 22:30:09 +0300

    smallest refactory

--- app/items.py
+++ app/items.py
@@ -5,16 +5,26 @@ from app.app_settings import dp
 from DB.models import Item
 
 
-@dp.route('/items', methods = ['GET', 'POST'])
+@dp.route('/items')
 @login_required
 def items():
     items = Item.get_items()
-    if request.method == 'POST':
-        words = request.form['itemSearch']
-        if words == '':
-            return redirect('/items')
-        words_list = [word.lower() for word in words.split()]
-        keywords = set(words_list)
-        search_items = Item.search_items(keywords = keywords)
-        return render_template('items.html', items_data = items, search_items_data = search_items)
     return render_template('items.html', items_data = items)
+
+@dp.route('/items', methods = ['POST'])
+@login_required
+def search_item():
+    items = Item.get_items()
+    words = request.form['itemSearch']
+    if words == '':
+        return redirect('/items')
+    words_list = [word.lower() for word in words.split()]
+    keywords = set(words_list)
+    search_items = Item.search_items(keywords = keywords)
+    return render_template('items.html', items_data = items, search_items_data = search_items)
+    
+# @dp.route('/items', method = ['POST'])
+# @login_required
+# def add_items_id_bascket():
+#     add_item = request.form['itemBuy']
+#     print(add_item)
