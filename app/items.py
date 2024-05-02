c9c92099ee147c1a003aacdd1c6173ae694e7853
Markinmarch <iztvmrk@mail.ru>
2024-05-02 23:03:19 +0300

    processing requests through error exceptions

--- app/items.py
+++ app/items.py
@@ -1,30 +1,24 @@
 from flask import request, render_template, redirect
 from flask_login import login_required
+from werkzeug.exceptions import BadRequestKeyError
 
 from app.app_settings import dp
 from DB.models import Item
 
 
-@dp.route('/items')
+@dp.route('/items', methods = ['GET', 'POST'])
 @login_required
 def items():
     items = Item.get_items()
+    if request.method == 'POST':
+        try:
+            words = request.form['itemSearch']
+            if words == '':
+                return redirect('/items')
+            words_list = [word.lower() for word in words.split()]
+            keywords = set(words_list)
+            search_items = Item.search_items(keywords = keywords)
+            return render_template('items.html', items_data = items, search_items_data = search_items)
+        except BadRequestKeyError:
+            return render_template('items.html', items_data = items)
     return render_template('items.html', items_data = items)
-
-@dp.route('/items', methods = ['POST'])
-@login_required
-def search_item():
-    items = Item.get_items()
-    words = request.form['itemSearch']
-    if words == '':
-        return redirect('/items')
-    words_list = [word.lower() for word in words.split()]
-    keywords = set(words_list)
-    search_items = Item.search_items(keywords = keywords)
-    return render_template('items.html', items_data = items, search_items_data = search_items)
-    
-# @dp.route('/items', method = ['POST'])
-# @login_required
-# def add_items_id_bascket():
-#     add_item = request.form['itemBuy']
-#     print(add_item)
