043b6ba95de802e759ad401fd6e9af681b21be4e
Markinmarch <iztvmrk@mail.ru>
2024-05-09 23:06:13 +0300

    some try with AJAX

--- app/items.py
+++ app/items.py
@@ -12,14 +12,14 @@ def items():
     items = Item.get_items()
     if request.method == 'POST':
         try:
-            words = request.form['itemSearch']
+            words = request.form['itemSearchInput']
             if words == '':
                 return redirect('/items')
             words_list = [word.lower() for word in words.split()]
             keywords = set(words_list)
             search_items = Item.search_items(keywords = keywords)
             return render_template('items.html', items_data = items, search_items_data = search_items)
-        except BadRequestKeyError:
+        except BadRequestKeyError or KeyError:
             item_id = request.form['itemId']
             item_count = request.form['itemCount']
             Order.add_items(
@@ -27,5 +27,33 @@ def items():
                 id_item_arg = item_id,
                 count_arg = item_count
             )
-            return render_template('items.html', items_data = items)
+            # return render_template('items.html', items_data = items, search_items_data = search_items)
     return render_template('items.html', items_data = items)
+
+
+# @dp.route('/items', methods = ['GET', 'POST'])
+# @login_required
+# def items():
+#     items = Item.get_items()
+#     if request.method == 'POST':
+#         # try:
+#         #     words = request.form['itemSearchInput']
+#         #     print(words)
+#         #     if words == '':
+#         #         return redirect('/items')
+#         #     words_list = [word.lower() for word in words.split()]
+#         #     keywords = set(words_list)
+#         #     search_items = Item.search_items(keywords = keywords)
+#         #     return render_template('items.html', items_data = items, search_items_data = search_items)
+#         # except KeyError:
+#             item_id = request.form['id']
+#             item_count = request.form['count']
+#             print(item_count)
+#             print(item_id)
+#             Order.add_items(
+#                 id_user_arg = current_user.get_id(),
+#                 id_item_arg = item_id,
+#                 count_arg = item_count
+#             )
+#             return render_template('items.html', items_data = items)
+#     return render_template('items.html', items_data = items)
---
+++ static/items.js
@@ -0,0 +1,26 @@
+// $(document).on('submit', '#itemSearch', function(elem) {
+//     console.log('hello');
+//     elem.preventDefault();
+//     $.ajax({
+//         type: "POST",
+//         url: "/items",
+//         data: {
+//             keywords: $("#itemSearchInput").val()
+//         },
+        // success: function() {
+        //     window.location.reload();
+        // }
+//     })
+// });
+
+// $(document).on('submit', '#itemBuy', function(elem){
+//     elem.preventDefault();
+//     $.ajax({
+//         type: "POST",
+//         url: "/items",
+//         data: {
+//             id: $("#itemId").val(),
+//             count: $("#itemCount").val()
+//         },
+//     })
+// });
