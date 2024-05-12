c00954209707a2df1aefd2a2d3c4f19d95bd7400
Markinmarch <iztvmrk@mail.ru>
2024-05-12 22:56:33 +0300

    learn JS + AJAX

--- app/items.py
+++ app/items.py
@@ -9,31 +9,21 @@ from DB.models import Item, Order
 @dp.route('/items', methods = ['GET', 'POST'])
 @login_required
 def items():
-    items = Item.get_items()
-    if request.method == 'POST':
-        try:
-            words = request.form['itemSearchInput']
-            if words == '':
-                return redirect('/items')
-            words_list = [word.lower() for word in words.split()]
-            keywords = set(words_list)
-            search_items = Item.search_items(keywords = keywords)
-            return render_template('items.html', items_data = items, search_items_data = search_items)
-        except BadRequestKeyError or KeyError:
-            item_id = request.form['itemId']
-            item_count = request.form['itemCount']
-            Order.add_items(
-                id_user_arg = current_user.get_id(),
-                id_item_arg = item_id,
-                count_arg = item_count
-            )
-            # return render_template('items.html', items_data = items, search_items_data = search_items)
-    return render_template('items.html', items_data = items)
+    # items = Item.get_items()
+    # search_items = request.args.get(key = 'keywords', default = None, type = str)
+    # print(search_items)
+    # if request.method == 'POST':
+    #     print(request.form['itemSearchInput'])
+        # print(request.form['id'])
+        # print(request.form['count'])
+        # return render_template('items.html', items_data = items)
+    return render_template('items.html')
 
 
-# @dp.route('/items', methods = ['GET', 'POST'])
-# @login_required
-# def items():
+# @dp.route('/items/<keywords:str>', methods = ['GET', 'POST'])
+# def search_items():
+#     search_items = request.args.get('keywords')
+#     print(search_items)
 #     items = Item.get_items()
 #     if request.method == 'POST':
 #         # try:
@@ -57,3 +47,11 @@ def items():
 #             )
 #             return render_template('items.html', items_data = items)
 #     return render_template('items.html', items_data = items)
+
+@dp.route('/items/search', methods = ['GET', 'POST'])
+def search_item():
+    keywords = request.args.get('keywords')
+    keywords_list = [keyword.lower() for keyword in keywords.split()]
+    set_keywords = set(keywords_list)
+    search_item = Item.search_items(keywords = keywords)
+    return render_template('items.html', search_items_data = search_item)
--- static/items.js
+++ static/items.js
@@ -1,19 +1,4 @@
-// $(document).on('submit', '#itemSearch', function(elem) {
-//     console.log('hello');
-//     elem.preventDefault();
-//     $.ajax({
-//         type: "POST",
-//         url: "/items",
-//         data: {
-//             keywords: $("#itemSearchInput").val()
-//         },
-        // success: function() {
-        //     window.location.reload();
-        // }
-//     })
-// });
-
-// $(document).on('submit', '#itemBuy', function(elem){
+// $(document).on('submit', '#itemBuyButton', function(elem){
 //     elem.preventDefault();
 //     $.ajax({
 //         type: "POST",
@@ -22,5 +7,83 @@
 //             id: $("#itemId").val(),
 //             count: $("#itemCount").val()
 //         },
+//         success: function() {
+//             window.location.reload();
+//         }
 //     })
 // });
+
+// $(document).on('submit', '#itemSearch', function(elem) {
+//     elem.preventDefault();
+//     $.ajax({
+//         type: "POST",
+//         url: "/items",
+//         data: {
+//             keywords: $("#itemSearchInput").val()
+//         },
+//         success: function() {
+//             window.location.reload();
+//         }
+//     })
+// });
+
+// document.getElementById("itemSearchButton").onclick = searchItem;
+
+// function searchItem() {
+//     var keywords = document.getElementById("itemSearchInput");
+//     window.location.replace("http://localhost:5000/items/search");
+//     var url = new URL(window.location.href);
+//     url.searchParams.set('single', keywords);
+//     window.location.href = url.href;
+    // if (keywords.value == "") {
+    //     return true;
+    // }
+
+    // else {
+    //     window.location.href += "?хуй"
+    // }
+// }
+
+window.onload = searchItem;
+
+function searchItem() {
+    var buttonName = document.getElementById("itemSearchButton");
+    buttonName.onclick = getSearchItems;
+}
+
+function getSearchItems() {
+    var keywords = document.getElementById("itemSearchInput");
+
+    if (keywords.value == ""){
+        alert("Введите ключевые слова, пожалуйста!");
+        return false;
+    }
+    window.location.replace("http://localhost:5000/items/search?keywords=" + keywords.value);
+    
+    // var url = new URL(window.location.href);
+    // url.searchParams.set('keywords', keywords.value);
+    // window.location.href = url.href;
+
+    // var xhr = new XMLHttpRequest();
+    // xhr.open("POST", "/registration", true);
+    // xhr.setRequestHeader('content-type', 'application/json; charset=UTF-8');
+    // xhr.send(userData);
+    // xhr.onload = function() {
+    //     if (xhr.response == "false") {
+    //         alert("Адрес почты "+ userEmail.value + " уже зарегестрирован. Используйте другой.");
+    //         userEmail.value = "";
+    //         return false;
+    //     }
+    //     else if (xhr.response == "few characters") {
+    //         alert("Пароль должен состоять минимум из 6 знаков");
+    //         userPassword.value = "";
+    //         repeatPassword.value = "";
+    //         return false;
+    //     }
+    //     else {
+    //         alert("Поздравляем, " + userName.value + ", регистрация завершена!");
+    //         window.location.replace("http://localhost:5000");
+    //     }
+    // }
+    
+}
