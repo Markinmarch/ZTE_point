88eaaae63681bb2773872b63abf59dab87ca9e01
Markinmarch <iztvmrk@mail.ru>
2024-06-10 22:40:54 +0300

    add bascket views

--- app/bascket.py
+++ app/bascket.py
@@ -1,11 +1,12 @@
 from flask import request, render_template, redirect
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 from app.app_settings import dp
-from DB.models import Item
+from DB.models import Item, Bascket
 
 
 @dp.route('/bascket')
 @login_required
 def bascket():
-    return render_template('bascket.html')
+    user_bascket = Bascket.not_paid_item_list(user_id = current_user.get_id())
+    return render_template('bascket.html', data = user_bascket)
