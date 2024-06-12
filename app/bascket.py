67d80d8f6fd8e346ce384fa6b9b35d6b8220fa7b
Markinmarch <iztvmrk@mail.ru>
2024-06-12 22:49:36 +0300

    rebuild structure view bascket

--- app/bascket.py
+++ app/bascket.py
@@ -3,14 +3,11 @@ from flask_login import login_required, current_user
 
 from app.app_settings import dp
 from DB.models import Item, Bascket
+from DB.main_db import session
 
 
 @dp.route('/bascket')
 @login_required
 def bascket():
     user_bascket = Bascket.not_paid_item_list(user_id = current_user.get_id())
-    ready_to_paid_item_list = []
-    for item_in_bascket in user_bascket:
-        data = Item.established_items_list(id = item_in_bascket['item_id'])
-        data_list = ready_to_paid_item_list.append(data)   
-    return render_template('bascket.html', data = data_list)
+    return render_template('bascket.html', data = user_bascket)
