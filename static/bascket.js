f54cfec63cd5425fc1b57fa2648e12a6fa6ccda8
Markinmarch <iztvmrk@mail.ru>
2024-06-18 22:47:44 +0300

    add items in bascket

--- static/bascket.js
+++ static/bascket.js
@@ -1,8 +1,36 @@
-$('.plus').click(function(){
-    var value =  $('#quantity').val();
-   $('#quantity').val(parseInt(value)+1);
+// AJAX, который отправляет параметры товара
+$(document).on("submit", "#itemBuy", function(event){
+    event.preventDefault();
+    var itemIndex = $(this).attr("itemIndex")
+    $.ajax({
+        type: "POST",
+        url: "/bascket",
+        data: {
+            id: $("#itemId" + itemIndex).val(),
+            count: $("#itemCount" + itemIndex).val()
+        },
+    });
+    // Добавляем стиль кнопке, при добавлении в корзину
+    $(".bascket").addClass("before-click-buy-button");
 });
-$('.minus').click(function(){
-    var value =  $('#quantity').val();
-   $('#quantity').val(parseInt(value)-1);
-});
+
+$(".plus").click(
+    function(event){
+        event.preventDefault();
+        var itemIndex = $(this).attr("itemIndex")
+        var plusOne =  $("#itemCount" + itemIndex).val();
+        $("#itemCount" + itemIndex).val(parseInt(plusOne)+1);
+    }
+);
+
+$(".minus").click(
+    function(event){
+        event.preventDefault();
+        var itemIndex = $(this).attr("itemIndex");
+        var minusOne =  $("#itemCount" + itemIndex).val();
+        if (minusOne == 1) {
+            return false;
+        }
+        $("#itemCount" + itemIndex).val(parseInt(minusOne)-1);
+    }
+);
