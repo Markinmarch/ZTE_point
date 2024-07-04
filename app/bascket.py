a328088b43faffeecbb7f3642a85c6bdda8221fb
Markinmarch <iztvmrk@mail.ru>
2024-07-04 22:18:27 +0300

    request via Flask without JS

--- app/bascket.py
+++ app/bascket.py
@@ -1,6 +1,8 @@
 from flask import request, render_template, redirect
 from flask_login import login_required, current_user
 
+from sqlalchemy.exc import NoResultFound
+
 from app.app_settings import dp
 from DB.models import Bascket
 
@@ -9,18 +11,21 @@ from DB.models import Bascket
 @login_required
 def bascket():
     if request.method == 'POST':
-        item_id = request.form['id']
-        Bascket.delete_item(
-            user_id = current_user.get_id(),
-            item_id = item_id
-        )
+        try:
+            item_id = request.form['itemId']
+            Bascket.delete_item(
+                user_id = current_user.get_id(),
+                item_id = item_id
+            )
+        except NoResultFound:
+            pass
     user_bascket = Bascket.not_paid_item_list(user_id = current_user.get_id())
+    print(user_bascket)
     if user_bascket == []:
         bascket_status = False
     else:
         bascket_status = True
     return render_template('bascket.html', data = user_bascket, bascket_status = bascket_status)
-
 # @dp.route('/bascket/payment', methods = ['POST', 'GET'])
 # @login_required
 # def payment():
